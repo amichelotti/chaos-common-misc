0d3772d5adbbfb0bd2d1317bd1f9633f149d3c07
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-02-02 23:33:25 +0100

    added pio support lib

---
+++ beagle/beagle_pio.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+int beagle_configure_pio(int pion,int dir){
+  char str[256];
+  FILE *f=fopen("/sys/class/gpio/export","w");
+  if(f==NULL){
+    printf("## cannot access to /sys/class/gpio/export, unable to configure\n");
+    return -1;
+  }
+  fprintf(f,"%d\n",pion);
+  fclose(f);
+  sprintf(str,"/sys/class/gpio/gpio%d/direction",pion);
+  f=fopen(str,"w");
+  if(f==NULL){
+    printf("## cannot access to %s, unable to configure directiob\n",str);
+    return -2;
+  }
+  if(dir==0){
+    fprintf(f,"in\n");
+  } else {
+    fprintf(f,"out\n");
+  }
+  fclose(f);
+  return 0;
+}
+/**
+   \brief set a value of an output pio
+   \param pion the pio number
+   \param dir 0 is input, 1 is output
+   \return 0 if success
+*/
+int beagle_pio_set(int pion,int val){
+  char str[256];
+  FILE*f;
+  sprintf(str,"/sys/class/gpio/gpio%d/value",pion);
+  f=fopen(str,"w");
+  if(f==NULL){
+    printf("## cannot access to %s, unable to set pio %d\n",str,pion);
+    return -2;
+  }
+  if(val)
+    fprintf(f,"1\n");
+  else
+    fprintf(f,"0\n");
+
+  fclose(f);
+  return 0;
+}
+
+int beagle_pio_get(int pion,int *val){
+  char str[256];
+  FILE*f;
+  sprintf(str,"/sys/class/gpio/gpio%d/value",pion);
+  f=fopen(str,"r");
+  if(f==NULL){
+    printf("## cannot access to %s, unable to read pio %d\n",str,pion);
+    return -2;
+  }
+  fscanf(f,"%d",val);
+  fclose(f);
+  return 0;
+}
---
+++ beagle/beagle_pio.h
@@ -0,0 +1,44 @@
+#ifndef __BEAGLE_PIO_H__
+#define __BEAGLE_PIO_H__
+/*
+ *	beagle_pio
+ *	!CHAOS
+ *	Created by Andrea Michelotti.
+ *
+ *    	Copyright 2013 INFN, National Institute of Nuclear Physics
+ *
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
+ *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
+ */
+
+/**
+   \brief configure a pio
+   \param pion the pio number
+   \param dir 0 is input, 1 is output
+   \return 0 if success
+*/
+int beagle_configure_pio(int pion,int dir);
+/**
+   \brief set a value of an output pio
+   \param pion the pio number
+   \param dir 0 is input, 1 is output
+   \return 0 if success
+*/
+int beagle_pio_set(int pion,int val);
+/**
+   \brief get a value of an input pio
+   \param pion the pio number
+   \param in val is returned the value 
+   \return 0 if success
+*/
+int beagle_pio_get(int pion,int *val);
+#endif
