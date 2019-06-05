8206ea46810755e5273c9f9d69c78ee62429c13a
aduffizi <alessandro.duffizi@lnf.infn.it>
2019-06-05 13:30:53 +0200

    added util lib for POST

---
+++ utility/HttpSender.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+namespace general
+{
+    namespace utility
+    {
+        class HTTPResponse
+        {
+            public:
+            int32_t ReturnCode;
+              std::string ReturnMessage;
+              std::string PayloadAnswer;
+            HTTPResponse() ;
+        };
+
+
+class HTTPClient
+{
+private :
+	  std::string host_address;
+	  std::string host_port;
+	
+		
+public:
+	
+	HTTPClient(std::string address, std::string port);
+	
+	HTTPResponse SendHttpPost(std::string api, std::string contentType, std::string payload);
+	
+    };
+}
+}
